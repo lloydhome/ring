b3b9fba41adc6eec7fe527ce02f2e99100dcbc39
lloydhome <david@lloydhome.com>
2010-07-04 23:50:13 -0700

    Fix issues with holding onto flash messages and deleting sessions

--- ring-core/src/ring/middleware/flash.clj
+++ ring-core/src/ring/middleware/flash.clj
@@ -1,7 +1,7 @@
 (ns ring.middleware.flash
   "A session-based flash store that persists to the next request.")
 
-(defn wrap-flash
+(defn wrap-flash-new
   "If a :flash key is set on the response by the handler, a :flash key with
   the same value will be set on the next request that shares the same session.
   This is useful for small messages that persist across redirects."
@@ -14,8 +14,10 @@
                      :session session
                      :flash flash)
           response (handler request)
+          session  (if (contains? response :session)
+                       (response :session)                             ;; use user provided session
+                       session)                                        ;; or the original
           session  (if-let [flash (response :flash)]
-                     (assoc (response :session session) :_flash flash))]
-      (if (or session (contains? response :session))
-        (assoc response :session session)
-        response))))
+                     (assoc (response :session session) :_flash flash) ;; set _flash into the stored session
+                     session)]                                         ;; or use the original for no set flash
+          (assoc response :session session))))
